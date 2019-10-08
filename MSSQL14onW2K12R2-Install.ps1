0c8bb4366250e2c1d796c576ce269685944f4283
dcasota <dcasota@gmail.com>
2019-10-08 20:37:13 +0200

    Add files via upload

---
+++ MSSQL14onW2K12R2-Install.ps1
@@ -0,0 +1,122 @@
+﻿#
+# Deploy the Azure Microsoft SKU  MS SQL Server 2014 on Windows Server 2012 R2
+#
+# History
+# 0.1  27.06.2019   dcasota  Initial release
+# 0.2  08.10.2019   dcasota 
+
+# SKU
+# https://docs.microsoft.com/en-us/azure/virtual-machines/windows/cli-ps-findimage
+
+# Location setting
+$LocationName = "westeurope"
+
+# Resourcegroup setting
+$ResourceGroupName = "azure-scripts-rg"
+
+# network setting
+$NetworkName = "mssql-lab"
+
+# virtual network and subnets setting
+$SubnetAddressPrefix = "192.168.1.0/24"
+$VnetAddressPrefix = "192.168.0.0/16"
+
+# VM setting
+$VMSize = "Standard_F4"
+$ComputerName = "mssql-01"
+$VMName = $ComputerName
+$NICName = $ComputerName + "nic"
+$VMLocalAdminUser = "LocalAdminUser"
+$VMLocalAdminPwd="Secure123!"
+$disk1SizeGB = "100"
+$disk2SizeGB = "75"
+$PublicIPDNSName="mypublicdns$(Get-Random)"
+$nsgName = "myNetworkSecurityGroup"
+$publisherName = "MicrosoftSQLServer"
+$offerName = "SQL2014SP2-WS2012R2"
+$skuName = "Enterprise"
+$marketplacetermsname= "Enterprise"
+$productversion = "12.21.190910"
+
+# Create az login object. You get a pop-up prompting you to enter the credentials.
+$cred = Get-Credential -Message "Enter a username and password for az login."
+connect-Azaccount -Credential $cred
+# Verify Login
+if( -not $(Get-AzContext) ) { return }
+
+# Verify VM doesn't exist
+[Microsoft.Azure.Commands.Compute.Models.PSVirtualMachine] `
+$VM = Get-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName -ErrorAction SilentlyContinue
+if (-not ([string]::IsNullOrEmpty($VM))) { return }
+
+# create lab resource group if it does not exist
+$result = get-azresourcegroup -name $ResourceGroupName -Location $LocationName -ErrorAction SilentlyContinue
+if (([string]::IsNullOrEmpty($result)))
+{
+    New-AzResourceGroup -Name $ResourceGroupName -Location $LocationName
+}
+
+$vnet = get-azvirtualnetwork -name $networkname -ResourceGroupName $resourcegroupname -ErrorAction SilentlyContinue
+if (([string]::IsNullOrEmpty($vnet)))
+{
+    $SingleSubnet  = New-AzVirtualNetworkSubnetConfig -Name frontendSubnet -AddressPrefix $SubnetAddressPrefix
+    $vnet = New-AzVirtualNetwork -Name $NetworkName -ResourceGroupName $ResourceGroupName -Location $LocationName -AddressPrefix $VnetAddressPrefix -Subnet $SingleSubnet
+    $vnet | Set-AzVirtualNetwork
+}
+
+
+# create vm
+# -----------
+
+# VM local admin setting
+$VMLocalAdminSecurePassword = ConvertTo-SecureString $VMLocalAdminPwd -AsPlainText -Force
+$LocalAdminUserCredential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword)
+
+# networksecurityruleconfig
+$nsg=get-AzNetworkSecurityGroup -Name $nsgName -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue
+if (([string]::IsNullOrEmpty($nsg)))
+{
+    $rdpRule = New-AzNetworkSecurityRuleConfig -Name myRdpRule -Description "Allow RDP" `
+    -Access Allow -Protocol Tcp -Direction Inbound -Priority 110 `
+    -SourceAddressPrefix Internet -SourcePortRange * `
+    -DestinationAddressPrefix * -DestinationPortRange 3389
+    $nsg = New-AzNetworkSecurityGroup -Name $nsgName -ResourceGroupName $ResourceGroupName -Location $LocationName -SecurityRules $rdpRule
+}
+# Create a public IP address
+$nic=get-AzNetworkInterface -Name $NICName -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue
+if (([string]::IsNullOrEmpty($nic)))
+{
+    $pip = New-AzPublicIpAddress -ResourceGroupName $ResourceGroupName -Location $LocationName -Name $PublicIPDNSName -AllocationMethod Static -IdleTimeoutInMinutes 4
+    # Create a virtual network card and associate with public IP address and NSG
+    $nic = New-AzNetworkInterface -Name $NICName -ResourceGroupName $ResourceGroupName -Location $LocationName `
+        -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
+}
+
+# marketplace plan
+# be aware first the marketplace terms must be accepted manually. https://github.com/terraform-providers/terraform-provider-azurerm/issues/1145#issuecomment-383070349
+
+# Create a virtual machine configuration
+$vmConfig = New-AzVMConfig -VMName $VMName -VMSize $VMSize | `
+Add-AzVMNetworkInterface -Id $nic.Id
+
+$vmimage= get-azvmimage -Location $LocationName -PublisherName $publisherName -Offer $offerName -Skus $skuName -Version $productversion
+if (!(([string]::IsNullOrEmpty($vmimage))))
+{
+    if (!(([string]::IsNullOrEmpty($vmimage.PurchasePlan))))
+    {
+        get-azmarketplaceterms -publisher $vmimage.PurchasePlan.publisher -Product $vmimage.PurchasePlan.product -name $vmimage.PurchasePlan.name
+        $agreementTerms=Get-AzMarketplaceterms -publisher $vmimage.PurchasePlan.publisher -Product $vmimage.PurchasePlan.product -name $vmimage.PurchasePlan.name
+        Set-AzMarketplaceTerms -publisher $vmimage.PurchasePlan.publisher -Product $vmimage.PurchasePlan.product -name $vmimage.PurchasePlan.name -Terms $agreementTerms -Accept
+        $vmConfig = Set-AzVMPlan -VM $vmConfig -publisher $vmimage.PurchasePlan.publisher -Product $vmimage.PurchasePlan.product -name $vmimage.PurchasePlan.name
+    }
+
+    $vmConfig = Set-AzVMOperatingSystem -Windows -VM $vmConfig -ComputerName $ComputerName -Credential $LocalAdminUserCredential | `
+    Set-AzVMSourceImage -PublisherName $publisherName -Offer $offerName -Skus $skuName -Version $productversion
+
+    # Create the VM
+    New-AzVM -ResourceGroupName $ResourceGroupName -Location $LocationName -VM $vmConfig
+}
+
+# Verify that the vm was created
+$vmList = Get-AzVM -ResourceGroupName $resourceGroupName
+$vmList.Name
